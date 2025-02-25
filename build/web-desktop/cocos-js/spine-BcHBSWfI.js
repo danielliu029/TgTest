8c30384cdf6216993983fbe076b88390d6489773
danielliu029 <bohan.liu029@gmail.com>
2025-02-25 15:56:40 +0800

    部署新代码

---
+++ build/web-desktop/cocos-js/spine-BcHBSWfI.js
@@ -0,0 +1,11 @@
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine = exports("default", 'assets/spine-u3j38j0v.wasm'); /* asset-hash:ac8c224c */
+
+		})
+	};
+}));
+//# sourceMappingURL=spine-BcHBSWfI.js.map
