0c2787b24e97b6c289be3b0e6b66352823993068
danielliu029 <bohan.liu029@gmail.com>
2025-03-05 14:14:59 +0800

    update code

--- build/web-desktop/index.a9014.js
+++ build/web-desktop/index.a9014.js
@@ -1,4 +1,4 @@
-System.register(["./application.f66b1.js"], function (_export, _context) {
+System.register(["./application.12b7e.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
